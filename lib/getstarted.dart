1c38210a02c7ff5543df03c5dbbe1a343aff7e26
Aliyaali1 <65674815+Aliyaali1@users.noreply.github.com>
2021-12-07 13:11:34 +0500

    Update getstarted.dart

---
+++ lib/getstarted.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import './login.dart';
+import './signup.dart';
+void main() {
+  runApp(const Start());
+
+}
+class Start extends StatelessWidget {
+  const Start({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Builder(
+        builder: (ctx)=>Scaffold(
+        appBar: AppBar(
+          backgroundColor:  const Color(0xFF001D4B),
+//          title: const Center(child: Text('Login Page')),
+        ),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(top: 50,left:50 ,right:50 ,bottom:10 ),
+                child: Text(
+                  "Lets Get Started ",
+                  style: TextStyle(
+                    letterSpacing: 1,
+                    fontSize:35,
+                    color: Color(0xFF001D4B),
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const Padding(padding: EdgeInsets.only(top: 0,left:0 ,right:0,bottom: 0)),
+              const Text(
+                "It is never late to get started!",
+                style: TextStyle(
+                  fontSize: 20,
+                  letterSpacing: 1,
+                  fontWeight: FontWeight.w400,
+                  color: Color(0xFF001D4B), ////
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.only(top: 150.0),
+                child: MaterialButton(onPressed: () {
+                  Navigator.push(
+                    ctx,
+                    MaterialPageRoute(builder: (context) => const Signup()),
+                  );
+
+                },
+                  height: 60,
+                  minWidth: 300,
+                  color: const Color(0xFF001D4B),
+                  shape:RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+
+                  child: const Text("Signup",style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 20,
+                    letterSpacing: 1,
+                    color: Colors.white,
+
+                  ),
+                  ),
+
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.only(top: 50.0),
+                child: MaterialButton(onPressed: () {
+                  Navigator.push(
+                    ctx,
+                    MaterialPageRoute(builder: (context) => const Login() ),
+                  );
+
+                },
+                  height: 60,
+                  minWidth: 300,
+                  color: const Color(0xFF001D4B),
+                  shape:RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+
+                  child: const Text("Login",style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 20,
+                    letterSpacing: 1,
+                    color: Colors.white,
+
+                  ),
+                  ),
+
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+    );
+  }
+}
